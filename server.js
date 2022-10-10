fdc01944ec70e7b153a26387a243dd94a4c88a20
Datist <mynameisthanhdat@gmail.com>
2022-10-10 21:14:55 +0700

    Change port

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const app = express()
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
